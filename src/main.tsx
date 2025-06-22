c236041b7fb309732bd810cc02cfdadcb57ce21b
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-06-22 13:24:06 -0300

    add a query client wrapping the App

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,18 @@ import "./index.css";
 import App from "./App.tsx";
 import { AppRouter } from "@/routing";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App>
-        <AppRouter />
-      </App>
+      <QueryClientProvider client={queryClient}>
+        <App>
+          <AppRouter />
+        </App>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 );
