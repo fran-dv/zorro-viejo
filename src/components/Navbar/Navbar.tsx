5889bda3b21ca8e0bdb05f541f893fabbc4d916b
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-06-22 00:02:19 -0300

    feat: make the navbar responsive with the sidebar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import { ShoppingBag } from "lucide-react";
 import { Paths } from "@/routing";
-import { CategoriesDropdown } from "@/components";
-
+import { CategoriesDropdown, MobileNavMenu } from "@/components";
+import { useMediaQuery } from "usehooks-ts";
 export const Navbar = () => {
+  const isSmallDevice = useMediaQuery("(max-width: 750px)");
+
   return (
     <nav className={styles.container}>
       <ul className={styles.linksContainer}>
@@ -13,14 +15,22 @@ export const Navbar = () => {
             <h2>Zorro Viejo</h2>
           </Link>
         </li>
-        <li className={styles.link}>
-          <CategoriesDropdown />
-        </li>
-        <li>
-          <Link className={styles.link} to={Paths.Cart}>
-            <ShoppingBag />
-          </Link>
-        </li>
+        {!isSmallDevice ? (
+          <>
+            <li className={styles.link}>
+              <CategoriesDropdown />
+            </li>
+            <li>
+              <Link className={styles.link} to={Paths.Cart}>
+                <ShoppingBag />
+              </Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <MobileNavMenu />
+          </>
+        )}
       </ul>
     </nav>
   );
