4c6aee15be7985715051a21f56fa9fcffc4abadb
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-06-24 15:52:13 -0300

    feat: implement basic products page

--- src/pages/public/Products/Products.tsx
+++ src/pages/public/Products/Products.tsx
@@ -1,7 +1,51 @@
+import { LoadingView } from "@/components";
+import { useProducts } from "@/hooks/useProducts";
+import type { Category } from "@/models";
+import { useSearchParams } from "react-router-dom";
+import { useGlobalContext } from "@/context";
+import { useMemo } from "react";
+import { ProductsList } from "@/components";
+
+const PRODUCTS_LIMIT = 10;
+
 export const Products = () => {
+  const { categories } = useGlobalContext();
+  const [searchParams] = useSearchParams();
+  const currentSlug = searchParams.get("category");
+  const currentCategory: Category | "all" =
+    categories.find((cat) => cat.slug === currentSlug) ?? "all";
+
+  const categoriesIdsToFetch: number[] = useMemo(
+    () =>
+      currentCategory === "all"
+        ? categories.map((cat) => cat.id)
+        : [currentCategory.id],
+    [currentCategory, categories],
+  );
+
+  const { data, error, isFetching } = useProducts({
+    limitPerCategory: PRODUCTS_LIMIT,
+    categoriesIds: categoriesIdsToFetch,
+  });
+
+  if (error) {
+    throw new Error(`Error fetching products: ${error.message}`);
+  }
+
+  if (!data && isFetching) {
+    return <LoadingView message="Cargando productos..." />;
+  }
+
+  const prodsByCategory = data?.map((item) => {
+    return {
+      category: categories.find((cat) => cat.id === item.categoryId)!,
+      products: item.products,
+    };
+  });
+
   return (
     <div>
-      <h1>Products</h1>
+      <ProductsList productsByCategory={prodsByCategory ?? []} />
     </div>
   );
 };
