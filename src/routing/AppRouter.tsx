8cf3dd92fa5959dce2661a7b7757bda5596ae203
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-06-30 12:17:20 -0300

    feat: implement routing for product detail page

--- src/routing/AppRouter.tsx
+++ src/routing/AppRouter.tsx
@@ -1,7 +1,11 @@
 import { Navigate, Route } from "react-router-dom";
-import { AdminLogin, Cart, Home, Products } from "@/pages/public";
-import { AdminGuard, Paths } from "@/routing";
-import { PrivateRouter, RoutesWithNotFound } from "@/routing";
+import { AdminLogin, Cart, Home, ProductDetail, Products } from "@/pages";
+import {
+  AdminGuard,
+  Paths,
+  PrivateRouter,
+  RoutesWithNotFound,
+} from "@/routing";
 
 export const AppRouter = () => {
   return (
@@ -9,6 +13,7 @@ export const AppRouter = () => {
       <Route path="/" element={<Navigate to={Paths.Home} />} />
       <Route path={Paths.Home} element={<Home />} />
       <Route path={Paths.Products} element={<Products />} />
+      <Route path={Paths.ProductDetail} element={<ProductDetail />} />
       <Route path={Paths.Cart} element={<Cart />} />
       <Route path={Paths.AdminLogin} element={<AdminLogin />} />
       <Route element={<AdminGuard />}>
--- src/routing/paths.ts
+++ src/routing/paths.ts
@@ -1,9 +1,11 @@
 export const Paths = {
   Home: "/home",
   Products: "/products",
+  ProductDetail: "/products/:slug",
   Cart: "/cart",
   AdminLogin: "/admin/login",
   AnyAdmin: "/admin/*",
   AdminDashboard: "dashboard",
   NotFound: "/404",
+  getProductDetailPath: (slug: string) => `/products/${slug}`,
 };
