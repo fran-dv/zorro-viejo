5054e6f88392792be753d03af9ac56ad930cd64b
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-07-01 11:50:51 -0300

    refactor: fetch similar products if the query fails with fetchProudctBySlug

--- src/api/fetchProductBySlug.ts
+++ src/api/fetchProductBySlug.ts
@@ -6,20 +6,44 @@ interface FetchOpts {
   slug: string;
 }
 
+interface fetchProductBySlugResponse {
+  product: Product | null;
+  similarProducts: Product[] | null;
+}
+
 export const fetchProductBySlug = async ({
   slug,
-}: FetchOpts): Promise<Product | null> => {
+}: FetchOpts): Promise<fetchProductBySlugResponse> => {
   const { data, error } = await supabase
     .from(Tables.Products)
     .select()
     .eq("slug", slug)
     .single();
 
-  if (error) {
-    throw new Error(`Network error: ${error.message}`);
+  if (!data || error) {
+    const { data, error } = await supabase
+      .from(Tables.Products)
+      .select()
+      .ilike("slug", `%${slug}%`)
+      .limit(10)
+      .order("name", { ascending: true });
+
+    if (error) {
+      throw new Error(`Network error: ${error.message}`);
+    }
+
+    const similarProducts = Array.isArray(data)
+      ? data.map((p, i) => parseProduct(p, i) as Product)
+      : [parseProduct(data, 0) as Product];
+
+    return {
+      product: null,
+      similarProducts: similarProducts.length > 0 ? similarProducts : null,
+    };
   }
 
-  if (!data) return null;
-
-  return parseProduct(data, 0);
+  return {
+    product: parseProduct(data, 0),
+    similarProducts: null,
+  };
 };
