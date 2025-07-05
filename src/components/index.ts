17158749ebb488b654b587e4f07cddbf97fd8f98
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-07-04 21:57:10 -0300

    fix: correct index.ts sticky footer bar exportation

--- src/components/index.ts
+++ src/components/index.ts
@@ -19,4 +19,4 @@ export * from "./EmblaCarousel/EmblaCarousel";
 export * from "./ImagesCarousel/ImagesCarousel";
 export * from "./ProductsCarousel/ProductsCarousel";
 export * from "./SearchInterface/SearchInterface";
-export * from "./StickyFooterButton/StickyFooterBar";
+export * from "./StickyFooterBar/StickyFooterBar";
