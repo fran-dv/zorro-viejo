abd36dfe11acfebfa8b61de26dd6b217e5b4298f
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-07-08 13:50:59 -0300

    feat: implement zustand global store for cart with data persistence

--- src/stores/useCartStore.ts
+++ src/stores/useCartStore.ts
@@ -1,5 +1,6 @@
 import type { CartItem, Product } from "@/models";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface CartState {
   items: CartItem[];
@@ -7,50 +8,75 @@ interface CartState {
   incrementItemAmount: (itemId: number) => boolean;
   decrementItemAmount: (itemId: number) => boolean;
   removeItem: (itemId: number) => boolean;
+  clearCart: () => void;
+  getTotalPrice: () => number;
 }
 
-export const useCartStore = create<CartState>((set, get) => ({
-  items: [],
-  addItemToCart: (product: Product) => {
-    const { items } = get();
-    const exists = items.some((it) => it.id === product.id);
-    if (exists) return false;
-    set((state) => ({
-      items: [...state.items, { id: product.id, amount: 1, product: product }],
-    }));
-    return true;
-  },
-  incrementItemAmount: (itemId: number) => {
-    const { items } = get();
-    const itemIndex = items.findIndex((it) => it.id === itemId);
-    if (itemIndex === -1) return false;
-    set((state) => ({
-      items: state.items.map((it) =>
-        it.id === itemId ? { ...it, amount: it.amount + 1 } : it,
-      ),
-    }));
-    return true;
-  },
-  decrementItemAmount: (itemId: number) => {
-    const { items } = get();
-    const itemIndex = items.findIndex((it) => it.id === itemId);
-    if (itemIndex === -1) return false;
-    set((state) => ({
-      items: state.items.map((it) =>
-        it.id === itemId
-          ? { ...it, amount: it.amount === 1 ? it.amount : it.amount - 1 }
-          : it,
-      ),
-    }));
-    return true;
-  },
-  removeItem: (itemId: number) => {
-    const { items } = get();
-    const itemIndex = items.findIndex((it) => it.id === itemId);
-    if (itemIndex === -1) return false;
-    set((state) => ({
-      items: state.items.filter((it) => it.id !== itemId),
-    }));
-    return true;
-  },
-}));
+export const useCartStore = create<CartState>()(
+  persist(
+    (set, get) => ({
+      items: [],
+      addItemToCart: (product: Product) => {
+        const { items } = get();
+        const exists = items.some((it) => it.id === product.id);
+        if (exists) return false;
+        set((state) => ({
+          items: [
+            ...state.items,
+            { id: product.id, amount: 1, product: product },
+          ],
+        }));
+        return true;
+      },
+      incrementItemAmount: (itemId: number) => {
+        const { items } = get();
+        const itemIndex = items.findIndex((it) => it.id === itemId);
+        if (itemIndex === -1) return false;
+        set((state) => ({
+          items: state.items.map((it) =>
+            it.id === itemId ? { ...it, amount: it.amount + 1 } : it,
+          ),
+        }));
+        return true;
+      },
+      decrementItemAmount: (itemId: number) => {
+        const { items } = get();
+        const itemIndex = items.findIndex((it) => it.id === itemId);
+        if (itemIndex === -1) return false;
+        set((state) => ({
+          items: state.items.map((it) =>
+            it.id === itemId
+              ? { ...it, amount: it.amount === 1 ? it.amount : it.amount - 1 }
+              : it,
+          ),
+        }));
+        return true;
+      },
+      removeItem: (itemId: number) => {
+        const { items } = get();
+        const itemIndex = items.findIndex((it) => it.id === itemId);
+        if (itemIndex === -1) return false;
+        set((state) => ({
+          items: state.items.filter((it) => it.id !== itemId),
+        }));
+        return true;
+      },
+      clearCart: () => {
+        set({
+          items: [],
+        });
+      },
+      getTotalPrice: () => {
+        const { items } = get();
+        return items.reduce((total, item) => {
+          const price = item.product.offerPrice ?? item.product.price;
+          return total + price * item.amount;
+        }, 0);
+      },
+    }),
+    {
+      name: "cart-storage",
+      partialize: (state) => ({ items: state.items }),
+    },
+  ),
+);
