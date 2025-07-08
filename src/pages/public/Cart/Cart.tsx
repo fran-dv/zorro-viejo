3d142d682f6558fc4667250e1e88fc5aa10c9019
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-07-08 13:51:13 -0300

    feat: implement the cart page

--- src/pages/public/Cart/Cart.tsx
+++ src/pages/public/Cart/Cart.tsx
@@ -1,7 +1,86 @@
+import { useCartStore } from "@/stores";
+import styles from "./Cart.module.css";
+import { NavigateButton } from "@/components";
+import { Paths } from "@/routing";
+import { CartItemCard, Checkout } from "./components";
+
+const title: React.ReactNode = (
+  <div className={styles.titleContainer}>
+    <h1>Tu carrito</h1>
+  </div>
+);
+
 export const Cart = () => {
+  const {
+    items,
+    incrementItemAmount,
+    decrementItemAmount,
+    removeItem,
+    getTotalPrice,
+  } = useCartStore();
+
+  const handleIncrementItem = (itemId: number) => {
+    incrementItemAmount(itemId);
+  };
+
+  const handleDecrementItem = (itemId: number) => {
+    decrementItemAmount(itemId);
+  };
+
+  const handleDeleteItem = (itemId: number) => {
+    removeItem(itemId);
+  };
+
+  if (items.length === 0) {
+    return (
+      <div className={styles.emptyContainer}>
+        {title}
+        <p>No hay productos en el carrito :(</p>
+        <NavigateButton
+          to={Paths.Products}
+          arrow
+          arrowSide="left"
+          className={styles.navigateButton}
+        >
+          Explorar bebidas
+        </NavigateButton>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Cart</h1>
-    </div>
+    <>
+      <div className={styles.returnButtonContainer}>
+        <NavigateButton
+          to={Paths.Products}
+          arrow
+          arrowSide="left"
+          className={styles.navigateButton}
+        >
+          Volver a bebidas
+        </NavigateButton>
+      </div>
+      {title}
+
+      <div className={styles.container}>
+        <ul className={styles.itemsList}>
+          {items.map((item) => (
+            <li key={item.id}>
+              <CartItemCard
+                item={item}
+                onIncrementItem={() => handleIncrementItem(item.id)}
+                onDecrementItem={() => handleDecrementItem(item.id)}
+                onDeleteItem={() => handleDeleteItem(item.id)}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <Checkout
+          total={getTotalPrice()}
+          className={styles.checkoutContainer}
+        />
+      </div>
+    </>
   );
 };
