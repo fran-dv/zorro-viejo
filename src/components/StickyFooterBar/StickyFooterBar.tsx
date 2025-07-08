86761d9b14820e295361e1362cdaa3b141afa238
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-07-08 13:47:19 -0300

    refactor: allow handle wrapper and container styles in StickyFooterBar

--- src/components/StickyFooterBar/StickyFooterBar.tsx
+++ src/components/StickyFooterBar/StickyFooterBar.tsx
@@ -3,12 +3,23 @@ import styles from "./StickyFooterBar.module.css";
 interface Props {
   children: React.ReactNode;
   isHidden?: boolean;
+  wrapperClassName?: string;
+  containerClassName?: string;
 }
 
-export const StickyFooterBar = ({ children, isHidden }: Props) => {
+export const StickyFooterBar = ({
+  children,
+  isHidden,
+  wrapperClassName = "",
+  containerClassName = "",
+}: Props) => {
   return (
-    <div className={`${styles.wrapper} ${isHidden ? styles.hidden : ""}`}>
-      <div className={styles.container}>{children}</div>
+    <div
+      className={`${styles.wrapper} ${isHidden ? styles.hidden : ""} ${wrapperClassName}`}
+    >
+      <div className={`${styles.container} ${containerClassName}`}>
+        {children}
+      </div>
     </div>
   );
 };
