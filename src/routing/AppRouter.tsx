82357c89b51ead44ac116ce26d4cd9a3cfadb4b0
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-07-10 19:56:03 -0300

    feat(routing): implement Checkout flow routing

--- src/routing/AppRouter.tsx
+++ src/routing/AppRouter.tsx
@@ -1,5 +1,13 @@
 import { Navigate, Route } from "react-router-dom";
-import { AdminLogin, Cart, Home, ProductDetail, Products } from "@/pages";
+import {
+  AdminLogin,
+  Cart,
+  Checkout,
+  CheckoutSuccess,
+  Home,
+  ProductDetail,
+  Products,
+} from "@/pages";
 import {
   AdminGuard,
   Paths,
@@ -15,6 +23,9 @@ export const AppRouter = () => {
       <Route path={Paths.Products} element={<Products />} />
       <Route path={Paths.ProductDetail} element={<ProductDetail />} />
       <Route path={Paths.Cart} element={<Cart />} />
+      <Route path={Paths.Checkout} element={<Checkout />}>
+        <Route path={Paths.CheckoutSuccess} element={<CheckoutSuccess />} />
+      </Route>
       <Route path={Paths.AdminLogin} element={<AdminLogin />} />
       <Route element={<AdminGuard />}>
         <Route index path={Paths.AnyAdmin} element={<PrivateRouter />} />
