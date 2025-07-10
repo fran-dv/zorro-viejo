2e3d6fa4623782c7b67b391f9272038934ddb380
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-07-10 12:23:17 -0300

    refactor(NaviigateButton): support additional props and external navigation

--- src/components/NavigateButton/NavigateButton.tsx
+++ src/components/NavigateButton/NavigateButton.tsx
@@ -4,24 +4,38 @@ import { useNavigate } from "react-router-dom";
 
 interface Props {
   children: React.ReactNode;
-  to: string;
+  to?: string;
   arrow?: boolean;
   arrowSide?: "left" | "right";
   className?: string;
+  onClick?: () => void;
+  replace?: boolean;
 }
 
 export const NavigateButton = ({
   children,
-  to,
+  to = undefined,
   arrow = false,
   arrowSide = "right",
   className,
+  onClick,
+  replace = false,
 }: Props) => {
   const navigate = useNavigate();
+
+  const handleClick = () => {
+    onClick?.();
+
+    if (to) {
+      navigate(to, { replace });
+    }
+  };
+
   return (
     <button
+      type="button"
       className={`${className} ${styles.button}`}
-      onClick={() => navigate(to)}
+      onClick={handleClick}
     >
       {arrow && arrowSide === "left" && <ArrowLeft className={styles.arrow} />}
       <p className={styles.buttonText}>{children}</p>
