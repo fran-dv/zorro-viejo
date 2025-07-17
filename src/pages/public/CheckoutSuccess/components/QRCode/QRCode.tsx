211d561042d8916199a28fe6a3f27e324b083a4c
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-07-17 11:01:33 -0300

    refactor(CheckoutSucces): move QR code with its own file and implement skeleton loading

---
+++ src/pages/public/CheckoutSuccess/components/QRCode/QRCode.tsx
@@ -0,0 +1,37 @@
+import { QRCodeSVG } from "qrcode.react";
+import styles from "./QRCode.module.css";
+import WhatsappLogo from "@/assets/whatsapp-logo.svg";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+interface Props {
+  link: string;
+  onClick: () => void;
+  isLoading: boolean;
+}
+
+const QR_SIZE = 300;
+
+export const QRCode = ({ link, onClick, isLoading }: Props) => {
+  if (isLoading) {
+    return <Skeleton className={styles.qr} width={QR_SIZE} height={QR_SIZE} />;
+  }
+
+  return (
+    <>
+      <QRCodeSVG
+        value={link}
+        className={styles.qr}
+        size={QR_SIZE}
+        title="QR para WhatsApp"
+        level="M"
+        onClick={onClick}
+      />
+      <img
+        className={styles.qrWhatsappLogo}
+        src={WhatsappLogo}
+        alt="Logo de WhatsApp"
+      />
+    </>
+  );
+};
