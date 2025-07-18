c460ccb399c0476cecefb25e9a4d3d15c6d656ee
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-07-18 13:05:59 -0300

    refactor(useCategories): correct network errors handling

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -1,21 +1,34 @@
 import { fetchCategories } from "@/api";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useNetworkStatus } from "@/hooks";
+import { useEffect } from "react";
 
 export const useCategories = () => {
-  const { isPending, error, data, isFetching, isLoading } = useQuery({
-    queryKey: ["categories"],
-    queryFn: async () => await fetchCategories(),
-    placeholderData: keepPreviousData,
-    staleTime: 60_000,
-    retry: 3,
-    retryDelay: (retryAttempt) => 1000 * Math.min(2 ** retryAttempt, 30),
-  });
+  const { isPending, error, isError, data, isFetching, isLoading, refetch } =
+    useQuery({
+      queryKey: ["categories"],
+      queryFn: async () => await fetchCategories(),
+      placeholderData: keepPreviousData,
+      staleTime: 60_000,
+      retry: 3,
+      retryDelay: (retryAttempt) => 1000 * Math.min(2 ** retryAttempt, 30),
+    });
+
+  const isOnline = useNetworkStatus();
+
+  useEffect(() => {
+    if (isOnline && (isError || !data)) {
+      refetch();
+    }
+  }, [isOnline, refetch, data, isError]);
 
   return {
     isPending,
     error,
+    isError,
     data,
     isFetching,
     isLoading,
+    refetch,
   };
 };
