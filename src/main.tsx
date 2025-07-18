5b7fac2dd4273e91e0d5c9d537383460ede0a3ae
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-07-18 13:17:47 -0300

    feat: wrap application with ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,22 @@ import { AppRouter } from "@/routing";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GlobalProvider } from "@/context";
+import ErrorBoundary from "./components/Errors/ErrorBoundary.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <GlobalProvider>
-          <App>
-            <AppRouter />
-          </App>
-        </GlobalProvider>
-      </QueryClientProvider>
+      <ErrorBoundary>
+        <QueryClientProvider client={queryClient}>
+          <GlobalProvider>
+            <App>
+              <AppRouter />
+            </App>
+          </GlobalProvider>
+        </QueryClientProvider>
+      </ErrorBoundary>
     </BrowserRouter>
   </StrictMode>,
 );
