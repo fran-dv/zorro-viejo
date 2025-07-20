01167bf531db7029cedc3359102afc45b8d69a45
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-07-20 11:43:38 -0300

    feat(AdminGuard): implement admin guard logic with refine hooks

--- src/routing/AdminGuard.tsx
+++ src/routing/AdminGuard.tsx
@@ -1,8 +1,18 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { Paths } from "@/routing";
+import { useIsAuthenticated } from "@refinedev/core";
+import { LoadingView } from "@/components";
 
 export const AdminGuard = () => {
-  const isAdmin = true;
+  const { isLoading: authLoading, data: authData } = useIsAuthenticated();
 
-  return isAdmin ? <Outlet /> : <Navigate to={Paths.AdminLogin} replace />;
+  if (authLoading) {
+    return <LoadingView message="Cargando sesión…" />;
+  }
+
+  if (!authData?.authenticated) {
+    return <Navigate to={Paths.AdminLogin} replace />;
+  }
+
+  return <Outlet />;
 };
