0934abab5a7df762dbfa86fe7ea228efd1f169cb
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-07-25 19:36:42 -0300

    feat: implement basic admin dashboard

--- src/pages/private/Admin/AdminDashboard/AdminDashboard.tsx
+++ src/pages/private/Admin/AdminDashboard/AdminDashboard.tsx
@@ -1,7 +1,54 @@
+import { useList } from "@refinedev/core";
+import styles from "./AdminDashboard.module.css";
+import { OrderSchema, type Order } from "@/models";
+import { useNavigate } from "react-router-dom";
+import { Paths } from "@/routing";
+import { LastOrders } from "./components";
+import { LoadingView } from "@/components";
+
 export const AdminDashboard = () => {
+  const { data, error, isLoading } = useList({
+    resource: "orders",
+    pagination: { current: 1, pageSize: 10 },
+    sorters: [
+      {
+        field: "created_at",
+        order: "desc",
+      },
+    ],
+    meta: {
+      select: `
+      id,
+      customer_name,
+      created_at,
+      order_items:order_items (
+        amount,
+        product:products (*)
+      )
+    `,
+    },
+  });
+  const navigate = useNavigate();
+
+  if (error) {
+    return <div>Error al cargar órdenes</div>;
+  }
+
+  if (isLoading) {
+    return <LoadingView message="Cargando información..." />;
+  }
+
+  const orders: Order[] = data.data.map((order) => OrderSchema.parse(order));
+
+  const handleRowClick = (id: string) => {
+    navigate(Paths.getAdminOrderDetail(id));
+  };
+
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
+    <div className={styles.container}>
+      <h1>Panel principal</h1>
+
+      <LastOrders orders={orders} onOrderRowClick={handleRowClick} />
     </div>
   );
 };
