42e296e0654471e98816afcd23b7f846f5b0008f
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-07-25 19:25:23 -0300

    install AG Grid

--- src/main.tsx
+++ src/main.tsx
@@ -7,14 +7,16 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GlobalProvider } from "@/context";
 import ErrorBoundary from "./components/Errors/ErrorBoundary.tsx";
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ErrorBoundary>
-    <BrowserRouter>
-      <ErrorBoundary>
+      <BrowserRouter>
         <QueryClientProvider client={queryClient}>
           <GlobalProvider>
             <App>
@@ -23,7 +25,6 @@ createRoot(document.getElementById("root")!).render(
           </GlobalProvider>
         </QueryClientProvider>
       </BrowserRouter>
-      </ErrorBoundary>
-    </BrowserRouter>
+    </ErrorBoundary>
   </StrictMode>,
 );
