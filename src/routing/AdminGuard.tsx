e9a01255368af6984bc30668d24eab0db4975e2b
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-07-30 21:24:35 -0300

    fix(AdminGuard): display error view if still loading session after 15 seconds

--- src/routing/AdminGuard.tsx
+++ src/routing/AdminGuard.tsx
@@ -2,14 +2,50 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { Paths } from "@/routing";
 import { useIsAuthenticated } from "@refinedev/core";
 import { LoadingView } from "@/components";
+import { useEffect, useState } from "react";
+import { ErrorFetching } from "@/components/Errors";
 
 export const AdminGuard = () => {
   const {
     isLoading: authLoading,
     data: authData,
     isError,
+    refetch,
   } = useIsAuthenticated();
   const location = useLocation();
+  const [timeoutPassed, setTimeoutPassed] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!authLoading || authData) return;
+
+    const timeout = setTimeout(() => {
+      if (!authData && authLoading) {
+        setTimeoutPassed(true);
+      }
+    }, 15000);
+
+    return () => clearTimeout(timeout);
+  }, [authData, authLoading, refetch, timeoutPassed]);
+
+  if (timeoutPassed && !authData) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ErrorFetching
+          message="Error al cargar la sesión. Revisa tu conexión y vuelve a intentarlo"
+          onRetry={() => {
+            refetch();
+            setTimeoutPassed(false);
+          }}
+        />
+      </div>
+    );
+  }
 
   if (authLoading) {
     return <LoadingView message="Cargando sesión…" />;
