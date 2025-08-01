26390990ca43567d701de819b4bc6a48a68e47b1
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-08-01 19:40:07 -0300

    refactor(Checkout): add error handling and UI feedback for order submission

--- src/pages/public/Checkout/Checkout.tsx
+++ src/pages/public/Checkout/Checkout.tsx
@@ -1,16 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import styles from "./Checkout.module.css";
 import { CheckoutForm } from "@/pages/public";
 import { Paths } from "@/routing";
-import { existingOrderIdKey, generateRawOrderToInsert } from "@/utils";
+import { generateRawOrderToInsert } from "@/utils";
 import { useNavigate } from "react-router-dom";
 import type {
   CheckoutFormValues,
   OrderCreate,
   OrderItemCreate,
 } from "@/models";
-import { useEffect } from "react";
-import { useSubmitOrder } from "@/hooks";
+import { useCallback, useEffect } from "react";
+import { useExistingOrder, useSubmitOrder } from "@/hooks";
 import { useCartStore } from "@/stores";
 import { generateRawOrderItemsToInsert } from "@/utils";
 import { TriangleAlert } from "lucide-react";
@@ -19,12 +19,24 @@ export const Checkout = () => {
   const navigate = useNavigate();
   const { items: cartItems } = useCartStore();
   const { mutate, isPending, isError, error } = useSubmitOrder();
-  const existingOrderId = localStorage.getItem(existingOrderIdKey);
-  useEffect(() => {
-    if (existingOrderId) {
-      navigate(Paths.getCheckoutSuccessPath(existingOrderId));
+  const { orderId, checkExistingOrderValidity } = useExistingOrder();
+
+  const handleSuccess = useCallback(() => {
+    if (!orderId) {
+      return;
     }
-  }, [existingOrderId, navigate]);
+    navigate(Paths.getCheckoutSuccessPath(orderId));
+  }, [navigate, orderId]);
+
+  useEffect(() => {
+    if (orderId) {
+      handleSuccess();
+    }
+  }, [orderId, navigate, handleSuccess]);
+
+  if (cartItems.length === 0) {
+    return <Navigate to={Paths.Cart} />;
+  }
 
   const handleFormSubmit = (formData: CheckoutFormValues) => {
     const orderData: OrderCreate = generateRawOrderToInsert(formData);
@@ -37,10 +49,8 @@ export const Checkout = () => {
       { rawOrder: orderData, rawOrderItems: orderItemsData },
       {
         onSuccess: () => {
-          setTimeout(
-            () => navigate(Paths.getCheckoutSuccessPath(orderData.id)),
-            5000,
-          );
+          checkExistingOrderValidity();
+          handleSuccess();
         },
       },
     );
@@ -65,7 +75,7 @@ export const Checkout = () => {
         </div>
       )}
 
-      {!existingOrderId ? (
+      {!orderId ? (
         <CheckoutForm onSubmit={handleFormSubmit} isSubmitting={isPending} />
       ) : (
         <Outlet />
