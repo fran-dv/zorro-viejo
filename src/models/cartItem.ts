55fe626c53fc30fb0605b166a09b4dea64eee540
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-08-04 20:02:42 -0300

    fix(CartItem): use ProductSchema in product field instead of creating a zod type

--- src/models/cartItem.ts
+++ src/models/cartItem.ts
@@ -1,13 +1,10 @@
-import type { Product } from "@/models";
 import { z } from "zod";
 import { ProductSchema } from "@/models";
-
-const Product: z.ZodType<Product> = ProductSchema;
 
 export const CartItemSchema = z.object({
   id: z.number(),
   amount: z.number(),
-  product: Product,
+  product: ProductSchema,
 });
 
 export type CartItem = z.infer<typeof CartItemSchema>;
