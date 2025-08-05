1bfd209238ed6f8c8510f7b6436c92ec2b3b3a70
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-08-05 19:19:27 -0300

    feat(NotFound): implement 404 page design

--- src/pages/public/NotFound/NotFound.tsx
+++ src/pages/public/NotFound/NotFound.tsx
@@ -1,7 +1,26 @@
+import { NavigateButton } from "@/components";
+import styles from "./NotFound.module.css";
+import notFoundImage from "@/assets/404.webp";
+import { Paths } from "@/routing";
+
 export const NotFound = () => {
   return (
-    <div>
-      <h1>404 Page not found</h1>
+    <div className={styles.container}>
+      <img
+        className={styles.image}
+        src={notFoundImage}
+        loading="lazy"
+        alt="Imagen de error 404"
+      />
+
+      <h1 className={styles.title}>¡Uy, esa página no existe!</h1>
+      <p className={styles.subtitle}>
+        Parece que el zorro dejó vacía esta página. Pero no te preocupes,
+        tenemos muchas bebidas esperándote.
+      </p>
+      <NavigateButton className={styles.navigateButton} to={Paths.Home}>
+        Volver al inicio
+      </NavigateButton>
     </div>
   );
 };
