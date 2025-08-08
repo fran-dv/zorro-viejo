dfdffc8c2d2b8606327a33943da8e7da38c02b59
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-08-08 10:51:29 -0300

    feat: implement placeholder CreateProduct and EditProduct pages

---
+++ src/pages/private/Admin/CreateProduct/CreateProduct.tsx
@@ -0,0 +1,3 @@
+export const CreateProduct = () => {
+  return <div>Crear producto</div>;
+};
--- src/pages/private/index.ts
+++ src/pages/private/index.ts
@@ -1,3 +1,5 @@
 export * from "./Admin/AdminDashboard/AdminDashboard";
 export * from "./Admin/OrderDetail/OrderDetail";
 export * from "./Admin/OrdersList/OrderList";
+export * from "./Admin/CreateProduct/CreateProduct";
+export * from "./Admin/EditProduct/EditProduct";
