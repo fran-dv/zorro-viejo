15d35a4fac86dd9cf522f8fe3f72cf881faa7831
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-08-17 14:53:43 -0300

    feat(Footer): implement footer information

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Footer.module.css";
+import logo from "@/assets/zorro-viejo.webp";
+import { getWhatsappLink } from "@/utils";
 
 export const Footer = React.forwardRef<HTMLElement>((_, ref) => {
+  const [whatsappLink, setWhatsappLink] = React.useState<string>("");
+  useEffect(() => {
+    (async () => {
+      setWhatsappLink(
+        await getWhatsappLink({
+          shorten: false,
+          userAgent: "auto",
+          message: "Hola! quería hacerte una consulta",
+        }),
+      );
+    })();
+  }, []);
   return (
     <footer className={styles.container} ref={ref}>
-      <h2>Footer</h2>
+      <section className={styles.brand}>
+        <img src={logo} className={styles.logo} alt="Zorro viejo" />
+        <h2 className={styles.title}>Zorro Viejo</h2>
+      </section>
+      <section className={styles.links}>
+        <h4>¿Tienes alguna duda?</h4>
+        <ul>
+          <li>
+            <a href={whatsappLink} target="_blank">
+              Contáctanos
+            </a>
+          </li>
+          <li>
+            <p>Chascomús, Buenos Aires</p>
+          </li>
+        </ul>
+      </section>
     </footer>
   );
 });
