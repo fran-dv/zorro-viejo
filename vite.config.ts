19af9b3eb2f03bab9cf74a45c900c643a54e2507
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-08-20 15:39:19 -0300

    install and configure react testing library and vitest for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -50,5 +51,10 @@ export default defineConfig({
     modules: {
       localsConvention: "camelCase",
     },
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
+    globals: true,
   },
 });
